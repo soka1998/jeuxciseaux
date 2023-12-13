1901c4921b467637d1f286319c8f9bdd5ed9b9a3
soka1998 <soka1998hr@gmail.com>
2023-12-13 09:10:19 +0100

    add some chnages

--- script.js
+++ script.js
@@ -4,20 +4,33 @@ const contentResult = document.getElementById("result");
 
 
 //Click Event on the Button 
+//Variables for game data
 const possibleChoices = document.querySelectorAll('button');
 let userChoice
 let result 
 let computerChoice
  
-possibleChoices.forEach(possibleChoice =>possibleChoice.addEventListener('click',(e)=>{
-    // recapitulate id of button clicked
-    userChoice = e.target.id;
-    //add image that correspond the choice 
-    // contentUserChoice.innterHTML = `<img src ="${userChoice}.png">`
+// possibleChoices.forEach(possibleChoice =>possibleChoice.addEventListener('click',(e)=>{
+//     // recapitulate id of button clicked
+//     userChoice = e.target.id;
+//     //add image that correspond the choice 
+//     // contentUserChoice.innterHTML = `<img src ="${userChoice}.png">`
 
-    genererate_choice_computer()
-    verify();
+//     genererate_choice_computer()
+//     verify();
     
+// }))
+//Event Listener for button clicks
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', () => {
+    userChoice = possibleChoice.id;
+
+    // recapitulate id of button clicked
+    
+    genererate_choice_computer();
+    verify();
+    // Move the image setting here
+    contentUserChoice.innerHTML = `<img src="img/${userChoice}.png">`;
+    contentComputerChoice.innerHTML = `<img src="img/${computerChoice}.png">`;
 }))
 
 //function to generate computer choice 
@@ -33,14 +46,17 @@ function genererate_choice_computer(){
     if(random === 3){// if random = à 1:
         computerChoice  ='scissors'
     }
+    //log the computer's choice to the console
     console.log(computerChoice);
+     //Update the innerHTML of elements to display images corresponding to choices
     contentUserChoice.innerHTML = `<img src="img/${userChoice}.png">`
+   
     // add img correspond to the choicce
    contentComputerChoice.innerHTML=`<img src="img/${computerChoice}.png">`
 
     
 }
-//function verify if the user wins or not 
+//function verify if the user wins or loses
 function verify(){
 if(userChoice == computerChoice){
     result ="Draw";
